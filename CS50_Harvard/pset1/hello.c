6dcacabed87934d53601048599a715937cb16054
breakdecycle <swylye@gmail.com>
2017-10-29 13:15:44 +0800

    added some comments

--- CS50_Harvard/pset1/hello.c
+++ CS50_Harvard/pset1/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+# void means the function does not take in any arguments
 int main(void)
 {
     printf("Hello, world!\n");
+    # \n to move the cursor to the following line
 }
